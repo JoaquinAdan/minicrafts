377346f6582b2426dad0522c1b7eef738e4c05d6
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-05-30 22:58:43 -0300

    trying other font in mui theme

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -1,15 +1,6 @@
-import { Roboto } from 'next/font/google'
 import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
-export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
-})
-
-// Create a theme instance.
 const themeCreated = (mode: 'light' | 'dark') => {
   return createTheme({
     palette: {
@@ -39,7 +30,7 @@ const themeCreated = (mode: 'light' | 'dark') => {
           }),
     },
     typography: {
-      fontFamily: roboto.style.fontFamily,
+      fontFamily: ['monospace'].join(','),
     },
   })
 }

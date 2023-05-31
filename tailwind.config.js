66b463754eff717d93b8a2b0a06a4407f7a330b7
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-05-30 22:45:39 -0300

    purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    // Add any dynamic classes here
+    './src/components/**/*.js',
+  ],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',

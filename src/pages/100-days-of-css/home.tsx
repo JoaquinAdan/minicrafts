c519d6b397ccf0b92cfc451d1a76a8e911229a71
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-06-03 18:35:27 -0300

    bucle que genera 100 botones y pushea a un array

--- src/pages/100-days-of-css/home.tsx
+++ src/pages/100-days-of-css/home.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { Button, Grid, Box } from '@mui/material'
 import Link from 'next/link'
 
-const buttonData = [
-  { path: '/100-days-of-css/day-1', text: '1' },
-  { path: '/100-days-of-css/day-2', text: '2' },
-  { path: '/100-days-of-css/day-3', text: '3' },
-  { path: '#', text: '4' },
-  { path: '#', text: '5' },
-  { path: '#', text: '6' },
-  { path: '#', text: '7' },
-  { path: '#', text: '8' },
-  { path: '#', text: '9' },
-  { path: '#', text: '10' },
-  { path: '#', text: '11' },
-]
+const generateButtonData = () => {
+  const buttonData = []
+
+  for (let i = 1; i <= 100; i++) {
+    const button = {
+      path: `/100-days-of-css/day-${i}`,
+      text: `${i}`,
+    }
+    buttonData.push(button)
+  }
+
+  return buttonData
+}
 
 const Home = () => {
   return (
@@ -22,10 +22,10 @@ const Home = () => {
       100 days of css
       <Grid
         container
-        columns={{ xs: 11, sm: 16, md: 30, lg: 50 }}
+        columns={{ xs: 11, sm: 16, md: 30, lg: 46 }}
         sx={{ gap: { xs: '5px', md: '10px' }, display: 'flex', justifyContent: 'center' }}
       >
-        {buttonData.map((data, index) => (
+        {generateButtonData().map((data, index) => (
           <Grid key={index} item xs={2} md={4}>
             <Link href={data.path}>
               <Button variant='outlined'>{data.text}</Button>

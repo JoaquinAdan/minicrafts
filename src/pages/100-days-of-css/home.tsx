9dff724acaf9309cd4a7778f596e801d2e7507a8
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-06-02 21:01:49 -0300

    structure and logic for redirections in 100daysofcss

--- src/pages/100-days-of-css/home.tsx
+++ src/pages/100-days-of-css/home.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import Link from 'next/link'
 
 const Home = () => {
-  return <div>100 days of css</div>
+  return (
+    <div>
+      100 days of css
+      <Link href='/100-days-of-css/dayOne'>
+        <Button>1</Button>
+      </Link>
+      <Link href='/100-days-of-css/dayTwo'>
+        <Button>2</Button>
+      </Link>
+    </div>
+  )
 }
 
 export default Home
